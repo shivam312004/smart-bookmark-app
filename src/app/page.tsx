f43a23b5edf138f25bf5e1d10335900c3ca71b01
shivam312004 <shivam.sahu.cse.2022@nist.edu>
2026-02-14 01:05:10 +0530

    fix production redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-"use client";
-
-import { supabase } from "../lib/supabase";
-
-export default function Home() {
-  const login = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: "http://localhost:3000/dashboard",
-      },
-    });
-  };
-
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <button
-        onClick={login}
-        className="bg-black text-white px-6 py-3 rounded-lg"
-      >
-        Login with Google
-      </button>
-    </div>
-  );
-}
+const login = async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo:
+        "https://smart-bookmark-61go0t1hx-shivams-projects-a57c6754.vercel.app/",
+    },
+  });
+};
